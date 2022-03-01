ff75bd67af1b7e48e6a06a02c131b9b0863ac3d7
AdamVondracek <Adam_Vondracek@protonmail.com>
2022-03-01 21:56:14 +0100

    added cmake, fixes

---
+++ examples/test.c
@@ -0,0 +1,8 @@
+#include <vector.h>
+
+int main()
+{
+    int t_size = 20;
+    vector test_vector = vector_create_empty(t_size);
+    return 0;
+}
--- src/vector.c
+++ src/vector.c
@@ -6,18 +6,18 @@ struct Vector
     vtype * arr;
 };
 
-void vec_fill(vector input_vector,vtype val)
+void vec_fill(vector * input_vector,vtype val)
 {
-    for(int i = 0; i < input_vector.size; ++i)
-        input_vector.arr[i] = val; 
+    for(int i = 0; i < (*input_vector).size; ++i)
+        (*input_vector).arr[i] = val; 
 }
 
-void vector_normalize(vector input_vector)
+void vector_normalize(vector * input_vector)
 {
-    vtype MAX = input_vector.arr[0];
+    vtype MAX = (*input_vector).arr[0];
     vtype curr;
-    for(int i = 0; i < input_vector.size; ++i){
-        curr = input_vector.arr[i];
+    for(int i = 0; i < (*input_vector).size; ++i){
+        curr = (*input_vector).arr[i];
         if (MAX < curr)
         {
             MAX = curr;
@@ -26,10 +26,10 @@ void vector_normalize(vector input_vector)
     scalar_multiplication(input_vector, 1/MAX);
 }
 
-void scalar_multiplication(vector input_vector, vtype val)
+void scalar_multiplication(vector * input_vector, vtype val)
 {
-    for(int i = 0; i < input_vector.size; ++i)
-        input_vector.arr[i] *= val; 
+    for(int i = 0; i < (*input_vector).size; ++i)
+        (*input_vector).arr[i] *= val; 
 }
 
 vector vector_create_empty(int size)
@@ -40,11 +40,11 @@ vector vector_create_empty(int size)
     return return_vector;
 }
 
-vector vector_cpy(vector input_vector)
+vector vector_cpy(const vector * input_vector)
 {
     vector return_vector;
-    return_vector.size = input_vector.size;
-    for(int i = 0; i < input_vector.size; ++i)
+    return_vector.size = (*input_vector).size;
+    for(int i = 0; i < (*input_vector).size; ++i)
         return_vector.arr[i] =  return_vector.arr[i];
 }
 
@@ -55,7 +55,7 @@ vector vector_addition(const vector A,const vector B, bool * failed)
     if (A.size != B.size)
         failed = 1; return return_vector;
     
-    return_vector.arr = malloc(A.size * sizeof(vector));
+    return_vector.arr = malloc(A.size * sizeof(vtype));
     for(int i = 0; i < A.size; ++i)
     {
         return_vector.arr[i] = A.arr[i] + A.arr[i];
@@ -71,7 +71,7 @@ vector dot_product(const vector A,const vector B, bool * failed)
     if (A.size != B.size)
         failed = 1; return return_vector;
 
-    return_vector.arr = malloc(A.size * sizeof(vector));
+    return_vector.arr = malloc(A.size * sizeof(vtype));
     for(int i = 0; i < A.size; ++i)
     {
         return_vector.arr[i] = A.arr[i] * A.arr[i];
